6d8d34b0a3ae36a5ae0665ef52917057b502d016
Guille-Avila <100650586+Guille-Avila@users.noreply.github.com>
2023-05-31 03:25:51 -0500

    Set comments in english

--- conway.cpp
+++ conway.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 using namespace sf;
 
-const int cellSize = 10; // Tamanio celda
+const int cellSize = 10; // Size cell
 const int width = 800;
 const int height = 600;
 const int numRows = height / cellSize;
 const int numCols = width / cellSize;
 
-// Matriz evaluar vecinas
+// Matrix to get neighbors
 const int offsets[][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
 int main() {
@@ -24,7 +24,7 @@ int main() {
   bool drawing = false; // variable dibujo
 
   // Grid ===================================
-  // Columnas
+  // Columns
   vector<Vertex> verticalLines;
   for (int i = 0; i < numCols; ++i) {
     Vertex line[] = {Vertex(Vector2f(i * cellSize, 0), Color(50, 50, 50)),
@@ -32,7 +32,7 @@ int main() {
     verticalLines.insert(verticalLines.end(), begin(line), end(line));
   }
 
-  // Filas
+  // Rows
   vector<Vertex> horizontalLines;
   for (int i = 0; i < numRows; ++i) {
     Vertex line[] = {Vertex(Vector2f(0, i * cellSize), Color(50, 50, 50)),
@@ -43,34 +43,34 @@ int main() {
   // Cells ===============
   VertexArray cells(Quads);
 
-  // inicio programa ===============================
+  // Start Program ===============================
   while (window.isOpen()) {
     Event event;
     while (window.pollEvent(event)) {
       if (event.type == Event::Closed)
         window.close();
 
-      // Interaccion pintar celda ==================
+      // Interaction draw cell ==================
       if (event.type == Event::MouseButtonPressed) {
         if (event.mouseButton.button == Mouse::Left) {
-          drawing = true; // Activar dibujo
+          drawing = true; // start drawing
         }
       }
 
       if (event.type == Event::MouseButtonReleased) {
         if (event.mouseButton.button == Mouse::Left) {
-          drawing = false; // Desactivar dibujo
+          drawing = false; // end drawing
         }
       }
 
-      // Movimiento raton
+      // Move mouse
       if (event.type == Event::MouseMoved) {
         if (drawing) {
-          // Coordenadas raton
+          // Coordinates mouse
           int x = event.mouseMove.x / cellSize;
           int y = event.mouseMove.y / cellSize;
           if (x >= 0 && x < numCols && y >= 0 && y < numRows) {
-            grid[y][x] = true; // Dibujar la celda
+            grid[y][x] = true; // draw cell
             newGrid[y][x] = true;
           }
         }
@@ -79,21 +79,21 @@ int main() {
 
     window.clear(Color::Black);
 
-    // Dibujar grid ===================================
+    // Draw grid ===================================
     window.draw(&verticalLines[0], verticalLines.size(), Lines);
     window.draw(&horizontalLines[0], horizontalLines.size(), Lines);
 
-    // Dibujar cells ===================================
+    // Draw cells ===================================
     cells.clear();
 
     for (int i = 0; i < numRows; ++i) {
       for (int j = 0; j < numCols; ++j) {
         if (grid[i][j]) {
-          // coordenadas de la celda
+          // Coordinates cells
           float x = j * cellSize;
           float y = i * cellSize;
 
-          // Agregar cells
+          // Add cells
           cells.append(Vertex(Vector2f(x, y), Color(200, 200, 200)));
           cells.append(Vertex(Vector2f(x + cellSize, y), Color(200, 200, 200)));
           cells.append(Vertex(Vector2f(x + cellSize, y + cellSize), Color(200, 200, 200)));
@@ -104,46 +104,46 @@ int main() {
 
     window.draw(cells);
 
-    // Calcular nuevo estado celdas ============================
+    // Calculate new state cells ============================
     if (clock.getElapsedTime() >= updateInterval) {
       for (int i = 0; i < numRows; ++i) {
         for (int j = 0; j < numCols; ++j) {
           int aliveNeighbors = 0;
 
-          // Contar vecinas vivas
+          // Count neighbors alive
           for (const auto &offset : offsets) {
             int neighborX = j + offset[1];
             int neighborY = i + offset[0];
 
-            // Verificar limites grid
+            // Verufy limits grid
             if (neighborX >= 0 && neighborX < numCols && neighborY >= 0 && neighborY < numRows &&
                 grid[neighborY][neighborX]) {
-              aliveNeighbors++; // add vecina
+              aliveNeighbors++; // add neighbor
             }
           }
 
-          // Aplicar reglas ===============================
+          // Rules ===============================
           if (grid[i][j]) {
-            // Celda viva
+            // cell alive
             if (aliveNeighbors < 2 || aliveNeighbors > 3) {
-              newGrid[i][j] = false; // Muere
+              newGrid[i][j] = false; // die
             } else {
-              newGrid[i][j] = true; // Vive
+              newGrid[i][j] = true; // life
             }
           } else {
             // Celda muerta
             if (aliveNeighbors == 3) {
-              newGrid[i][j] = true; // Nace
+              newGrid[i][j] = true; // born
             }
           }
         }
       }
     }
 
-    // Actualizar grid
+    // Update grid
     if (clock.getElapsedTime() > updateInterval) {
       memcpy(grid, newGrid, sizeof(grid));
-      clock.restart(); // Reiniciar el contador de tiempo
+      clock.restart(); // Restart countdown
     }
 
     window.display();
